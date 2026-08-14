274840b518ef11193214ae786f13e9c1c02501e7
OutilsHSE <outilshse@gmail.com>
2026-08-14 10:26:36 +0200

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const CONFIG_ACCUEIL = {
   /* URL de la Web App Apps Script (déploiement « Application Web »).
      Laisser vide pour désactiver la sauvegarde : la fiche continue
      de fonctionner normalement, seul l'export PDF est disponible. */
-  apiUrl: "",
+  apiUrl: "https://script.google.com/macros/s/AKfycby8OfxjUUR_ID8uuJFUSAN6nXMW4-2XyjSkK6Sff0dyhBoK7mV8a2Z1GpxBlC0PHw4z0g/exec",
 
   /* Identifiant de l'outil dans le registre commun CDES */
   ficheId: "accueil-hse",
