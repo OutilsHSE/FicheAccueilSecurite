016fe42e2efb9479d8cf8e5f06019cdd61bcdb51
OutilsHSE <outilshse@gmail.com>
2026-08-14 10:52:38 +0200

    Update apiUrl to point to the new Web App

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const CONFIG_ACCUEIL = {
   /* URL de la Web App Apps Script (déploiement « Application Web »).
      Laisser vide pour désactiver la sauvegarde : la fiche continue
      de fonctionner normalement, seul l'export PDF est disponible. */
-  apiUrl: "",
+  apiUrl: "https://script.google.com/macros/s/AKfycbzgBaIkG7u-_LJ_uK-TtPWZjHfQxa3sUefOac-DoqIujsB8RE8jis1C8awgoWpvjO_vcw/exec",
 
   /* Identifiant de l'outil dans le registre commun CDES */
   ficheId: "accueil-hse",
